a2d026a3a440f1d35f5015c9a7696b3ade006e5c
ikvarxt <leonekola@outlook.com>
2025-02-23 01:26:56 +0800

    setup react plugin build

---
+++ reactnative/react-native.config.js
@@ -0,0 +1,12 @@
+/** @type import("@react-native-community/cli-types").Config */
+module.exports = {
+  project: {
+    ios: {},
+    android: {
+      packageName: 'com.reactandroidprojectstructuredemo',
+      sourceDir: '../',
+    },
+  },
+  root: './',
+  reactNativePath: './node_modules/react-native'
+};
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    includeBuild("reactnative/node_modules/@react-native/gradle-plugin")
+}
+plugins {
+    id("com.facebook.react.settings")
+}
+extensions.configure<com.facebook.react.ReactSettingsExtension> {
+    autolinkLibrariesFromCommand(
+        workingDirectory = settings.layout.rootDirectory.dir("reactnative").asFile,
+        lockFiles = settings.layout.rootDirectory
+            .dir("reactnative")
+            .files("yarn.lock", "package-lock.json", "package.json", "react-native.config.js")
+    )
+}
+rootProject.name = "ReactAndroidProjectStructureDemo"
+include (":app")
+includeBuild("reactnative/node_modules/@react-native/gradle-plugin")
